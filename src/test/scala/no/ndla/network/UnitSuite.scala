d1cd2af0662152d5a40a9b69fc4871635c973bf8
Kenneth Stigen <kes@knowit.no>
2016-07-21 09:38:10 +0200

    NDLA-377: Removed redundant import-statement

--- src/test/scala/no/ndla/network/UnitSuite.scala
+++ src/test/scala/no/ndla/network/UnitSuite.scala
@@ -8,7 +8,7 @@
 
 package no.ndla.network
 
-import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, _}
+import org.scalatest._
 import org.scalatest.mock.MockitoSugar
 
 abstract class UnitSuite extends FunSuite with Matchers with OptionValues with Inside with Inspectors with MockitoSugar with BeforeAndAfterAll with BeforeAndAfterEach
