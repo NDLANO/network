3c9cfcf6e98458d50bb9fd6cfcaee08bb0a4184f
Henrik Nårstad <henrik.narstad@knowit.no>
2016-11-08 13:44:30 +0100

    NDLA-443: Add option to retrieve more secrets

--- src/main/scala/no/ndla/network/secrets/Secrets.scala
+++ src/main/scala/no/ndla/network/secrets/Secrets.scala
@@ -8,24 +8,37 @@
 
 package no.ndla.network.secrets
 
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
-import com.amazonaws.regions.{Region, Regions}
-import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
+import no.ndla.network.secrets.PropertyKeys._
 import org.json4s.native.Serialization.read
 
 import scala.io.Source
 import scala.util.{Properties, Success, Try}
 
 object Secrets {
-  def readSecrets(secretsFile: String): Try[Map[String, Option[String]]] = {
-    val amazonClient = new AmazonS3Client(new DefaultAWSCredentialsProviderChain())
-    amazonClient.setRegion(Region.getRegion(Regions.EU_CENTRAL_1))
+  private val amazonClient = AmazonS3ClientBuilder.standard().withRegion(Regions.EU_CENTRAL_1).build()
+  val DBKeys = Set(MetaUserNameKey, MetaPasswordKey, MetaResourceKey, MetaServerKey, MetaPortKey, MetaSchemaKey)
 
-    new Secrets(amazonClient, Properties.envOrElse("NDLA_ENVIRONMENT", "local"), secretsFile).readSecrets()
+  def readSecrets(secretsFile: String): Try[Map[String, Option[String]]] =
+    new Secrets(amazonClient, Properties.envOrElse("NDLA_ENVIRONMENT", "local"), secretsFile, DBKeys).readSecrets()
+
+  def readSecrets(secretsFile: String, secretsKeysToRead: Set[String], readDBCredentials: Boolean = true): Try[Map[String, Option[String]]] = {
+    val keysToRead = secretsKeysToRead ++ (if (readDBCredentials) DBKeys else Set())
+    new Secrets(amazonClient, Properties.envOrElse("NDLA_ENVIRONMENT", "local"), secretsFile, keysToRead).readSecrets()
   }
 }
 
-class Secrets(amazonClient: AmazonS3Client, environment: String, secretsFile: String) {
+object PropertyKeys {
+  val MetaUserNameKey = "META_USER_NAME"
+  val MetaPasswordKey = "META_PASSWORD"
+  val MetaResourceKey = "META_RESOURCE"
+  val MetaServerKey = "META_SERVER"
+  val MetaPortKey = "META_PORT"
+  val MetaSchemaKey = "META_SCHEMA"
+}
+
+class Secrets(amazonClient: AmazonS3, environment: String, secretsFile: String, secretKeysToRead: Set[String]) {
 
   def readSecrets(): Try[Map[String, Option[String]]] = {
     implicit val formats = org.json4s.DefaultFormats
@@ -36,8 +49,9 @@ class Secrets(amazonClient: AmazonS3Client, environment: String, secretsFile: St
         for {
           s3Object <- Try(amazonClient.getObject(s"$environment.secrets.ndla", secretsFile))
           fileContent <- Try(Source.fromInputStream(s3Object.getObjectContent).getLines().mkString)
-          dbSecrets <- Try(read[Map[String, Option[String]]](fileContent))
-        } yield dbSecrets
+          allSecrets <- Try(read[Map[String, String]](fileContent))
+          secrets <- Try(secretKeysToRead.map(key => key -> allSecrets.get(key)).toMap)
+        } yield secrets
       }
     }
   }
--- src/test/scala/no/ndla/network/secrets/SecretsTest.scala
+++ src/test/scala/no/ndla/network/secrets/SecretsTest.scala
@@ -24,13 +24,14 @@ class SecretsTest extends UnitSuite {
   val amazonClient = mock[AmazonS3Client]
   val s3Object = mock[S3Object]
 
-  val ValidFileContent = """ {
-                             "database": "database-name",
-                             "host": "database-host",
-                             "user": "database-user",
-                             "password": "database-password",
-                             "port": "1234",
-                             "schema": "database-schema"
+  val ValidFileContent = s"""{
+                             "${PropertyKeys.MetaResourceKey}": "database-name",
+                             "${PropertyKeys.MetaServerKey}": "database-host",
+                             "${PropertyKeys.MetaUserNameKey}": "database-user",
+                             "${PropertyKeys.MetaPasswordKey}": "database-password",
+                             "${PropertyKeys.MetaPortKey}": "1234",
+                             "${PropertyKeys.MetaSchemaKey}": "database-schema",
+                             "GOOGLE_API_KEY": "ABCD1234"
                            }""".stripMargin
 
   override def beforeEach(): Unit = {
@@ -39,33 +40,52 @@ class SecretsTest extends UnitSuite {
   }
 
   test("That empty Map is returned when env is local") {
-    new Secrets(amazonClient, "local", "secretsFile").readSecrets() should equal (Success(Map()))
+    new Secrets(amazonClient, "local", "secretsFile", Secrets.DBKeys).readSecrets() should equal (Success(Map()))
   }
 
   test("That Map containing details about database is returned when env is test") {
     when(s3Object.getObjectContent).thenReturn(new S3ObjectInputStream(new ByteArrayInputStream(ValidFileContent.getBytes("UTF-8")), mock[HttpRequestBase]))
-    val secretsAttempt = new Secrets(amazonClient, "test", "secretsFile").readSecrets()
+    val secretsAttempt = new Secrets(amazonClient, "test", "secretsFile", Secrets.DBKeys).readSecrets()
 
     secretsAttempt match {
       case Failure(err) => fail(err)
       case Success(secrets) => {
-        secrets("user") should equal (Some("database-user"))
-        secrets("password") should equal (Some("database-password"))
-        secrets("database") should equal (Some("database-name"))
-        secrets("host") should equal (Some("database-host"))
-        secrets("port") should equal (Some("1234"))
-        secrets("schema") should equal (Some("database-schema"))
+        secrets(PropertyKeys.MetaUserNameKey) should equal (Some("database-user"))
+        secrets(PropertyKeys.MetaPasswordKey) should equal (Some("database-password"))
+        secrets(PropertyKeys.MetaResourceKey) should equal (Some("database-name"))
+        secrets(PropertyKeys.MetaServerKey) should equal (Some("database-host"))
+        secrets(PropertyKeys.MetaPortKey) should equal (Some("1234"))
+        secrets(PropertyKeys.MetaSchemaKey) should equal (Some("database-schema"))
+        secrets.get("GOOGLE_API_KEY") should equal (None)
+      }
+    }
+  }
+
+  test("That Map containing details about database and API key is returned when env is test") {
+    when(s3Object.getObjectContent).thenReturn(new S3ObjectInputStream(new ByteArrayInputStream(ValidFileContent.getBytes("UTF-8")), mock[HttpRequestBase]))
+    val secretsAttempt = new Secrets(amazonClient, "test", "secretsFile", Set("GOOGLE_API_KEY") ++ Secrets.DBKeys).readSecrets()
+
+    secretsAttempt match {
+      case Failure(err) => fail(err)
+      case Success(secrets) => {
+        secrets(PropertyKeys.MetaUserNameKey) should equal (Some("database-user"))
+        secrets(PropertyKeys.MetaPasswordKey) should equal (Some("database-password"))
+        secrets(PropertyKeys.MetaResourceKey) should equal (Some("database-name"))
+        secrets(PropertyKeys.MetaServerKey) should equal (Some("database-host"))
+        secrets(PropertyKeys.MetaPortKey) should equal (Some("1234"))
+        secrets(PropertyKeys.MetaSchemaKey) should equal (Some("database-schema"))
+        secrets("GOOGLE_API_KEY") should equal (Some("ABCD1234"))
       }
     }
   }
 
   test("That Failure is returned when not able to parse secret content") {
     when(s3Object.getObjectContent).thenReturn(new S3ObjectInputStream(new ByteArrayInputStream("ThouShaltNotParse".getBytes("UTF-8")), mock[HttpRequestBase]))
-    new Secrets(amazonClient, "test", "secretsFile").readSecrets().isFailure should be (true)
+    new Secrets(amazonClient, "test", "secretsFile", Secrets.DBKeys).readSecrets().isFailure should be (true)
   }
 
   test("That Failure is returned when Amazon-problems occur") {
     when(s3Object.getObjectContent).thenThrow(new RuntimeException("AmazonProblem"))
-    new Secrets(amazonClient, "test", "secretsFile").readSecrets().isFailure should be (true)
+    new Secrets(amazonClient, "test", "secretsFile", Secrets.DBKeys).readSecrets().isFailure should be (true)
   }
 }
