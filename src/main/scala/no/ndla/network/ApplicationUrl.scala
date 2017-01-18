f30e4dbc2fa9c49e3158d78554c3b5d9414b96ea
Rune Storløpa <rst@knowit.no>
2017-01-18 16:58:41 +0100

    #174: Bruker header fra ELB for å bestemme protokoll på URL'er

--- src/main/scala/no/ndla/network/ApplicationUrl.scala
+++ src/main/scala/no/ndla/network/ApplicationUrl.scala
@@ -11,16 +11,22 @@ package no.ndla.network
 import javax.servlet.http.HttpServletRequest
 
 object ApplicationUrl {
+  val ProtocolHeader = "X-Forwarded-Proto"
+  val HTTP = "http"
+  val HTTPS = "https"
   val HTTP_PORT = 80
   val HTTPS_PORT = 443
 
   val applicationUrl = new ThreadLocal[String]
 
   def set(request: HttpServletRequest) {
-    if (request.getServerPort != HTTP_PORT && request.getServerPort != HTTPS_PORT)
-      applicationUrl.set(s"${request.getScheme}://${request.getServerName}:${request.getServerPort}${request.getServletPath}/")
-    else
+    val protocolHeader = request.getHeader(ProtocolHeader)
+    if (protocolHeader != null && (protocolHeader.equals(HTTP) || protocolHeader.equals(HTTPS)))
+      applicationUrl.set(s"${protocolHeader}://${request.getServerName}${request.getServletPath}/")
+    else if (request.getServerPort == HTTP_PORT || request.getServerPort == HTTPS_PORT)
       applicationUrl.set(s"${request.getScheme}://${request.getServerName}${request.getServletPath}/")
+    else
+      applicationUrl.set(s"${request.getScheme}://${request.getServerName}:${request.getServerPort}${request.getServletPath}/")
   }
 
   def get: String = applicationUrl.get
