38e6e3671052558c6956e6d4b019a2879a078f67
Christian Peter <christian.peter@knowit.no>
2017-02-14 13:48:36 +0100

    Removes https in network

--- src/main/scala/no/ndla/network/Domains.scala
+++ src/main/scala/no/ndla/network/Domains.scala
@@ -5,7 +5,7 @@ object Domains {
   def get(environment: String): String = Map(
     "local" -> "http://proxy.ndla-local",
     "prod" -> "https://api.ndla.no"
-  ).getOrElse(environment, s"https://$environment.api.ndla.no")
+  ).getOrElse(environment, s"http://$environment.api.ndla.no")
 
 
 }
