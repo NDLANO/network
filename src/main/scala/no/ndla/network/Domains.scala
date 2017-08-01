88383e2f9cfb455d151a5fa9657e5004f6b57d72
narhen <henrik.narstad@knowit.no>
2017-08-01 13:14:07 +0200

    #465: update after proxy was removed

--- src/main/scala/no/ndla/network/Domains.scala
+++ src/main/scala/no/ndla/network/Domains.scala
@@ -3,7 +3,7 @@ package no.ndla.network
 object Domains {
 
   def get(environment: String): String = Map(
-    "local" -> "http://proxy.ndla-local",
+    "local" -> "http://api-gateway.ndla-local",
     "prod" -> "https://api.ndla.no"
   ).getOrElse(environment, s"https://$environment.api.ndla.no")
 
