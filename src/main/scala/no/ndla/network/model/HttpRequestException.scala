d90aabc1df6072d4c8bcca64f5916acb824b50ad
Henrik Nårstad <henrik.narstad@knowit.no>
2017-11-13 16:28:45 +0100

    make httpresponse in exception publicly accessible

--- src/main/scala/no/ndla/network/model/HttpRequestException.scala
+++ src/main/scala/no/ndla/network/model/HttpRequestException.scala
@@ -10,8 +10,8 @@ package no.ndla.network.model
 
 import scalaj.http.HttpResponse
 
-class HttpRequestException(message: String, httpResponse: Option[HttpResponse[String]] = None) extends RuntimeException(message) {
-  def is404:Boolean = httpResponse.exists(_.isCodeInRange(404, 404))
+class HttpRequestException(message: String, val httpResponse: Option[HttpResponse[String]] = None) extends RuntimeException(message) {
+  def is404: Boolean = httpResponse.exists(_.isCodeInRange(404, 404))
 }
 
 class AuthorizationException(message: String) extends RuntimeException(message)
