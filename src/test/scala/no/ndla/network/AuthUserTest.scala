7b9dcd8b2ebc55bfaa586125fd780a05f7d4ddea
Henrik Nårstad <henrik.narstad@knowit.no>
2018-11-21 15:02:37 +0100

    backwards compatability in extractUserRoles function #1439

---
+++ src/test/scala/no/ndla/network/AuthUserTest.scala
@@ -0,0 +1,25 @@
+/*
+ * Part of NDLA network.
+ * Copyright (C) 2018 NDLA
+ *
+ * See LICENSE
+ *
+ */
+
+package no.ndla.network
+
+class AuthUserTest extends UnitSuite {
+
+  test("getAuth0HostForEnv should return correct hostnames for environments") {
+    AuthUser.getAuth0HostForEnv("test") should equal("ndla-test.eu.auth0.com")
+    AuthUser.getAuth0HostForEnv("spoletest") should equal("ndla-test.eu.auth0.com")
+    AuthUser.getAuth0HostForEnv("brukertest") should equal("ndla-test.eu.auth0.com")
+    AuthUser.getAuth0HostForEnv("local") should equal("ndla-test.eu.auth0.com")
+    AuthUser.getAuth0HostForEnv("staging") should equal("ndla-staging.eu.auth0.com")
+    AuthUser.getAuth0HostForEnv("prod") should equal("ndla.eu.auth0.com")
+  }
+
+  test("getAuth0HostForEnv should return the test hostname for any undefined environments") {
+    AuthUser.getAuth0HostForEnv("tullball") should equal("ndla-test.eu.auth0.com")
+  }
+}
